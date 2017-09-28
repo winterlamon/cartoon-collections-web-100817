903b5895df204ee5009ce41aad9b650968ed6961
Winter LaMon <winter.lamon@gmail.com>
2017-09-28 15:06:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,12 +5,10 @@ def roll_call_dwarves(list_of_dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-#  captain_call =
   planeteer_calls.collect do |call|
     call.capitalize << "!"
   end
 end
-#  captain_call
 
 #def long_planeteer_calls(call_array)
   # Your code here
