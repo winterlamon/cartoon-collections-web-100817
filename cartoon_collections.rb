863f4118243d0fc401d3c856467cb4f8a714a045
Winter LaMon <winter.lamon@gmail.com>
2017-09-28 16:17:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    array.include?(cheese_types)
+    array.include?(cheese_types[0..-1])
     #puts "it found the cheese"
 #    array.find do |element|
 #      element == cheese_types[0..-1]
