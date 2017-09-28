8bb88b90fe30370db24d31aae42d4383d10c46bb
Winter LaMon <winter.lamon@gmail.com>
2017-09-28 16:03:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,8 @@ def find_the_cheese(array)
   if array.include?(cheese_types) == false
     return nil
   else
-    array.find(cheese_types) 
+    array.find do |element|
+      element == cheese_types[0..-1]
+    end
   end
 end
